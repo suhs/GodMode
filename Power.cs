a72c2079c82c9fdfd763a1507daa94e029ecd9f0
suhs <sophie.suh@maruvcrc.com>
2017-08-01 16:58:36 -0700

    just minor update on power

--- Power.cs
+++ Power.cs
@@ -5,8 +5,11 @@ public class Power {
   // prob. of overall power. Higher this value, more chance your action overriding result
   protected float willPower { get; set; }
 
-  // prob. of performing each action
+  // prob. of performing each action. These three SHOULD SUM UP TO 1
+  //
   protected float constructionPower { get; set; }
   protected float destructionPower { get; set; }
   protected float observationPower { get; set; }
+
+
 }
